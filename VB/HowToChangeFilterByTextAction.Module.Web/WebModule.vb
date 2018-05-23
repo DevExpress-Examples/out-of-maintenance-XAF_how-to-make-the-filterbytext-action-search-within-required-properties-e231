b2c8376b11fb1db102f6f150b36db99a200f3022
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ VB/HowToChangeFilterByTextAction.Module.Web/WebModule.vb
@@ -0,0 +1,15 @@
+﻿Imports System
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace HowToChangeFilterByTextAction.Module.Web
+   <ToolboxItemFilter("Xaf.Platform.Web")>
+   Public NotInheritable Partial Class HowToChangeFilterByTextActionAspNetModule
+       Inherits ModuleBase
+
+      Public Sub New()
+         InitializeComponent()
+      End Sub
+   End Class
+End Namespace
---
+++ VB/HowToChangeFilterByTextAction.Module/MyFilterController.vb
@@ -0,0 +1,42 @@
+﻿Imports System
+Imports System.ComponentModel
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Text
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Actions
+Imports DevExpress.Persistent.Base
+
+Imports DevExpress.ExpressApp.SystemModule
+
+Namespace HowToChangeFilterByTextAction.Module
+    Partial Public Class MyFilterController
+        Inherits ViewController
+
+        Public Sub New()
+            InitializeComponent()
+            RegisterActions(components)
+            Me.TargetObjectType = GetType(DevExpress.Persistent.BaseImpl.Person)
+        End Sub
+
+        Private Sub MyFilterController_Activated(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Activated
+            Dim standardFilterController As FilterController = Frame.GetController(Of FilterController)()
+            If standardFilterController IsNot Nothing Then
+                AddHandler standardFilterController.CustomGetFullTextSearchProperties, AddressOf standardFilterController_CustomGetFullTextSearchProperties
+            End If
+        End Sub
+
+        Private Sub standardFilterController_CustomGetFullTextSearchProperties(ByVal sender As Object, ByVal e As CustomGetFullTextSearchPropertiesEventArgs)
+            For Each [property] As String In GetFullTextSearchProperties()
+                e.Properties.Add([property])
+            Next [property]
+            e.Handled = True
+        End Sub
+        Private Function GetFullTextSearchProperties() As List(Of String)
+            Dim searchProperties As New List(Of String)()
+            searchProperties.Add("LastName")
+            Return searchProperties
+        End Function
+    End Class
+End Namespace
