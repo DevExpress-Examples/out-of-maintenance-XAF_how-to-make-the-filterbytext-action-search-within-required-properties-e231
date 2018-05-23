c5c2ebe7011ef996b2a25ac6a831f02978649579
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ CS/HowToChangeFilterByTextAction.Win/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Security;
+using DevExpress.ExpressApp.Win;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+
+namespace HowToChangeFilterByTextAction.Win {
+   static class Program {
+      /// <summary>
+      /// The main entry point for the application.
+      /// </summary>
+      [STAThread]
+      static void Main() {
+         Application.EnableVisualStyles();
+         Application.SetCompatibleTextRenderingDefault(false);
+         EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached;
+         HowToChangeFilterByTextActionWindowsFormsApplication application = new HowToChangeFilterByTextActionWindowsFormsApplication();
+         if (ConfigurationManager.ConnectionStrings["ConnectionString"] != null) {
+            application.ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+         }
+         try {
+            DevExpress.ExpressApp.InMemoryDataStoreProvider.Register();
+                        application.ConnectionString = DevExpress.ExpressApp.InMemoryDataStoreProvider.ConnectionString;
+            application.Setup();
+            application.Start();
+         }
+         catch (Exception e) {
+            application.HandleException(e);
+         }
+      }
+   }
+}
