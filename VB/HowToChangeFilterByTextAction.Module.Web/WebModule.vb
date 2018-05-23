267753b98e746e13985bf061b2fb88cd49940f86
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ VB/HowToChangeFilterByTextAction.Module.Web/WebModule.vb
@@ -0,0 +1,15 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.ComponentModel
+
+Imports DevExpress.ExpressApp
+
+Namespace HowToChangeFilterByTextAction.Module.Web
+   <ToolboxItemFilter("Xaf.Platform.Web")> _
+   Public NotInheritable Partial Class HowToChangeFilterByTextActionAspNetModule
+	   Inherits ModuleBase
+	  Public Sub New()
+		 InitializeComponent()
+	  End Sub
+   End Class
+End Namespace
